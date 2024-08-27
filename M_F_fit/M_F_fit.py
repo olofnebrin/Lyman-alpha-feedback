f7bdd3fd6b3128b95d99d84efd75abd981689d13
olofnebrin <128363074+olofnebrin@users.noreply.github.com>
2024-08-27 07:41:31 +0200

    Update M_F_fit.py

--- M_F_fit/M_F_fit.py
+++ M_F_fit/M_F_fit.py
@@ -5,89 +5,107 @@ from LyaDestruction import *
 from ConstantsParameters import *
 
 
-def M_F_fit(N_H, T, D_Dsun, taubar_s = 0.0, vmax = 0.0, f_H2 = 0.0, xHII = 0.0,
-            nH = 1e-5, z = 0.0, sigma_dHsun = 0.941e-21):
+def M_F_fit(N_HI_average, T, D_Dsun, taubar_s = 0.0, vmax = 0.0, N_HI_harmonic = None,
+            N_H2_average = 0.0, N_HII_average = 0.0, nHI_average = 1e-8, nHeI_average = 0.0,
+            nH2_average = 0.0, z = 0.0, sigma_dHsun = sigma_dHsun_HD23):
+    
     """ The Lya force multiplier. Arguments:
 
-        N_H:         Total hydrogen (HI+HII+H2) column density (cm^-2)
-        T:           Temperature (K)
-        taubar_s:    Relative size of emission region in optical depth
-        D_Dsun:      Dust-to-gas ratio in Solar units
-        vmax:        Cloud expansion/contraction velocity (km/s)
-        f_H2:        Molecular hydrogen fraction (for both column & number density, default = 0.0)
-        xHII:        Ionization fraction (default = 0.0)
-        nH:          Total hydrogen (HI + HII + H2) number density (cm^-3)
-        nHeI:        He I number density (cm^-3)
-        z:           Redshift (default = 0.0)
-        sigma_dHsun: Dust absorption cross-section at Lya at Solar dust abundance,
-                     per H nucelon (cm^2/H) (default = Hensley & Draine 2023) """
+        N_HI_average:  Solid-angle average HI column density (cm^-2).
+        
+        T:             Temperature (K).
+        
+        taubar_s:      Relative size of emission region in optical depth.
+        
+        D_Dsun:        Dust-to-gas ratio in Solar units.
+        
+        vmax:          Cloud expansion/contraction velocity (km/s).
+        
+        N_HI_harmonic: Harmonic solid-angle average HI column density (cm^-2). Only different from
+                       N_HI_average for anisotropic clouds. If left unspecified, it is assumed
+                       to equal N_HI_average.
 
-    # Column & number densities:
+        N_H2_average:  Solid-angle average H2 column density (cm^-2).
 
-    N_HI       = N_H*(1.0 - xHII - 2.0*f_H2)
-    N_H2       = f_H2*N_H
+        N_HII_average: Solid-angle average H II column density (cm^-2).
 
-    nHI        = nH*(1.0 - xHII - 2.0*f_H2)
-    nH2        = f_H2*nH
-    nHeI       = nH*Y/(4.*(1.0-Y))
+        nHI_average:   HI number density (cm^-3), weighted by solid-angle average HI column density.
+
+        nHeI_average:  He I number density (cm^-3), weighted by solid-angle average HI column density.
+
+        nH2_average:   H2 number density (cm^-3), weighted by solid-angle average HI column density.
+        
+        z:             Redshift (default = 0.0).
+        
+        sigma_dHsun:   Dust absorption cross-section at Lya at Solar dust abundance,
+                       per H nucelon (cm^2/H) (default = Hensley & Draine 2023). """
+
+    if N_HI_harmonic is None:
+        N_HI_harmonic = N_HI_average
+
 
     # Quantities and variables of interest:
 
-    T_100      = T/100.0                      # Convenient temperature normalization
+    T_100       = T/100.0                      # Convenient temperature normalization
 
-    a_v        = 4.7e-3*(T_100**(-0.5))       # Voigt parameter
-    sigma0     = 5.88e-13*(T_100**(-0.5))     # Lya cross-section @ line center
-    tau_cl     = sigma0*N_HI                  # Lya optical depth @ line center
-    atau       = a_v*tau_cl
+    a_v         = 4.7e-3*(T_100**(-0.5))       # Voigt parameter
+    sigma0      = 5.88e-13*(T_100**(-0.5))     # Lya cross-section @ line center
+    tau_cl_A    = sigma0*N_HI_average          # Lya optical depth @ line center, solid-angle averaged
+    tau_cl_H    = sigma0*N_HI_harmonic         # Lya optical depth @ line center, harmonic solid-angle averaged
+    tau_cl_AH   = np.sqrt(tau_cl_A*tau_cl_H)   # Geometric mean of tau_cl_A & tau_cl_H
+    eta_tau     = tau_cl_H/tau_cl_A            # Ratio between harmonic & standard solid-angle avearge optical depths
+    atauA       = a_v*tau_cl_A
 
     # Factor related to spatial source distribution:
 
-    taustar    = np.maximum( 1e-8, taubar_s )
-    eta_s      = 3.51*np.exp(-0.674*(taustar**2.42) - 1.21*(taustar**0.414))
+    taustar     = np.maximum( 1e-8, taubar_s )
+    eta_s       = 3.51*np.exp(-0.674*(taustar**2.42) - 1.21*(taustar**0.414))
 
     # Factors related to the Lya destruction probability:
 
-    P          = np.sqrt(6.*np.pi)*p_d(nHI, nHeI, nH2, T, z)*tau_cl/2.0
+    P           = np.sqrt(6.*np.pi)*p_d(nHI_average, nHeI_average, nH2_average, T, z)*tau_cl_AH/2.0
 
-    Pcritpoint = 1.8*np.exp(-(taustar**0.25))
-    Pcritext   = 1.5*(taustar**(-0.6))*np.exp(-10.0*taustar) + 3.6
+    Pcritpoint  = 1.8*np.exp(-(taustar**0.25))
+    Pcritext    = 1.5*(taustar**(-0.6))*np.exp(-10.0*taustar) + 3.6
 
     # Recoil-related factors:
 
-    xbar       = 2.54e-3*(T_100**(-0.5))      # Recoil parameter
-    xt1        = 1.207*(atau**(1/3))*xbar 
+    xbar        = 2.54e-3*(T_100**(-0.5))      # Recoil parameter
+    xt1         = 1.207*(eta_tau**(1/6))*(atauA**(1/3))*xbar 
 
-    xt1_crit   = 0.55*np.exp(-5.8*(taustar**0.5)) + 0.98
-    crec       = 0.54*np.exp(-1e3*taustar) + 0.63*(1.0-np.exp(-1e3*taustar))
-    eta_rec    = 0.35 + 0.65*(taustar**1.5)/(taustar**1.5 + 0.00395)
-    Grec       = 1.0 + eta_rec*(0.2*((xt1/xt1_crit)**2) + 0.005*((xt1/xt1_crit)**4))
+    xt1_crit    = 0.55*np.exp(-5.8*(taustar**0.5)) + 0.98
+    crec        = 0.54*np.exp(-1e3*taustar) + 0.63*(1.0-np.exp(-1e3*taustar))
+    eta_rec     = 0.35 + 0.65*(taustar**1.5)/(taustar**1.5 + 0.00395)
+    Grec        = 1.0 + eta_rec*(0.2*((xt1/xt1_crit)**2) + 0.005*((xt1/xt1_crit)**4))
 
 
     # Factors related to continuum absorption:
 
-    epst1      = 0.4707*(atau**(1./3.))*tau_cabs(D_Dsun, N_H, f_H2, sigma_dHsun)
+    N_H_average = N_HI_average + N_HII_average + 2*N_H2_average
 
-    cabs       = ((1.0-np.exp(-20.0*taustar))/(3.0*(1.0 + (P/50)**0.25 + (P/300)**0.75))
-               + 0.17*np.exp(-20.0*taustar) )
-    eta_abs    = 1.1*(1.0 - np.exp(-6.54*(taustar**1.06)))
-    Fabs       = ( (3.4*epst1)**cabs + (eta_abs*epst1)**(4*cabs))**(3/(4*cabs))
+    epst1       = 0.4707*(eta_tau**(2/3))*(atauA**(1/3))*tau_cabs(D_Dsun, N_H_average, N_H2_average, sigma_dHsun)
+
+    cabs        = ((1.0-np.exp(-20.0*taustar))/(3.0*(1.0 + (P/50)**0.25 + (P/300)**0.75))
+                + 0.17*np.exp(-20.0*taustar) )
+    eta_abs     = 1.1*(1.0 - np.exp(-6.54*(taustar**1.06)))
+    Fabs        = ( (3.4*epst1)**cabs + (eta_abs*epst1)**(4*cabs))**(3/(4*cabs))
 
     # Factors related to velocity gradients:
 
-    b          = np.sqrt(2*kB*T/mH)/kms       # Thermal velocity (km/s)
-    Rdot       = np.abs( vmax )/b             # Dimensionless expansion/contraction rate
+    b           = np.sqrt(2*kB*T/mH)/kms           # Thermal velocity (km/s)
+    Rdot        = (eta_tau**0.5)*np.abs( vmax )/b  # Effective dimensionless expansion/contraction rate
 
-    cvel       = 0.6/(1.0 + (P/1e3)**0.5)
-    eta_vel    = 1.0 - np.exp(- (taustar/0.1)**0.7)
-    Gvel       = 0.058*(Rdot**0.65)*(1.0-np.exp(-9.5*(taustar**0.75)))*np.exp(-0.02*(Rdot**0.6))
-    Fvel       = ((1.0 + 0.75*eta_vel*Rdot)**(2*cvel/3) - 1.0)**(1/cvel)
+    cvel        = 0.6/(1.0 + (P/1e3)**0.5)
+    eta_vel     = 1.0 - np.exp(- (taustar/0.1)**0.7)
+    Gvel        = 0.058*(Rdot**0.65)*(1.0-np.exp(-9.5*(taustar**0.75)))*np.exp(-0.02*(Rdot**0.6))
+    Fvel        = ((1.0 + 0.75*eta_vel*Rdot)**(2*cvel/3) - 1.0)**(1/cvel)
 
     # Put it all together:
 
     A   = 1.0 + (P/(Grec*Pcritpoint)) + (xt1/xt1_crit)**(3*crec) + Fabs + 0.29*Rdot
 
-    M_F = eta_s*(atau**(1/3))/(
+    M_F = eta_s*(eta_tau**(2/3))*(atau_A**(1/3))/(
           A**(1/3) + (P/((Grec+Gvel)*Pcritext)) + Fvel)
 
     return M_F
-                    
+     
